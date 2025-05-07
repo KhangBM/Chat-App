df4b1556c89e95c4583f45d9955291ff5338ad99
KhangBM <nguyenquockhang120901@gmail.com>
2025-05-07 20:04:13 +0700

    final

---
+++ app/src/main/java/com/example/projectchat/listeners/ConversionListener.java
@@ -0,0 +1,7 @@
+package com.example.projectchat.listeners;
+
+import com.example.projectchat.models.User;
+
+public interface ConversionListener {
+    void onConversionClicked(User user);
+}
---
+++ app/src/main/java/com/example/projectchat/listeners/UserListener.java
@@ -0,0 +1,7 @@
+package com.example.projectchat.listeners;
+
+import com.example.projectchat.models.User;
+
+public interface UserListener {
+    void onUserClicked (User user);
+}
